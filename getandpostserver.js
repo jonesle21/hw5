ae0a84ce06836e52ca90cf8ad9b587818422b92d
jonesle21 <67295425+jonesle21@users.noreply.github.com>
2021-02-25 00:43:35 -0500

    Update getandpostserver.js

--- getandpostserver.js
+++ getandpostserver.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-var handlerbars = require('express-handlebars').create({defaultLayout:'main'});
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
