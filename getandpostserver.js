6dbe774d0ba0a27b23bab9b3d757afd1064cda46
jonesle21 <67295425+jonesle21@users.noreply.github.com>
2021-02-24 23:43:30 -0500

    Update getandpostserver.js
    updated for post and get functions

--- getandpostserver.js
+++ getandpostserver.js
@@ -1,25 +1,52 @@
 var express = require('express');
 
 var app = express();
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var handlerbars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 12193);
 
-app.get('/HW5',function(req,res){
-    var info = [];
+//for post request
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/hw5', function(req,res){
+  var qParams = [];
+  for (var p in req.body){
+    qParams.push({'name':p,'value':req.body[p]})
+  }
+  console.log(qParams);
+  console.log(req.body);
+  var context = {};
+  context.dataList = qParams;
+  res.render('post', context);
+});
+
+//get request
+app.get('/hw5', function(req,res){
+    var qParams = [];
     for (var p in req.query){
-        info.push({'name':p, 'value':req.query[p]})
+        qParams.push({'name':p, 'value':req.query[p]})
     }
-    var datainput = {};
-    datainput.dataList = info
-  res.render('get.handlebars', datainput) //We can omit the .handlebars extension as we do below
+    var context = {};
+    context.dataList = qParams;
+    res.render('get', context)
 });
 
 app.use(function(req,res){
+  res.type('text/plain');
   res.status(404);
-  res.render('404');
+  res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
 });
 
 app.listen(app.get('port'), function(){
